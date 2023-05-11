68ba94005a2c2f656b76a22fbc49eec0ab2572e4
Suheb Ahmed <suhebsmac@Suhebs-MacBook-Pro.local>
2023-05-12 03:37:01 +0530

    8th commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 import './App.css';
 import Form from '../src/components/Form';
+import MobileNavbar from './components/MobileNavbar';
 function App() {
   
   return (
     <div className="App">
       <Form/>
+      <MobileNavbar/>
      
     </div>
   );
---
+++ src/components/MobileNavbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import bottomNavbar from '../assets/bottomNavbar.png' 
+
+const MobileNavbar = () => {
+  return (
+    <div className='fixed bottom-0 '  >
+        {/* <img src={bottomNavbar} alt="" /> */}
+        Mobile Navbar
+        </div>
+  )
+}
+
+export default MobileNavbar
